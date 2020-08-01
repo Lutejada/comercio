9c180e04cd4367e7e6d999e04e501af2a7696135
Luis Tejada <luetejava@gmail.com>
2020-08-01 15:18:33 -0500

    codigo inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { HeaderPromotionComponent } from './modules/header-promotion/header-promotion.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { NewletterComponent } from './modules/newletter/newletter.component';
+import { HeaderMobileComponent } from './modules/header-mobile/header-mobile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderPromotionComponent,
+    FooterComponent,
+    NewletterComponent,
+    HeaderMobileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/header-promotion/header-promotion.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Path} from '../../config';
+
+@Component({
+  selector: 'app-header-promotion',
+  templateUrl: './header-promotion.component.html',
+  styleUrls: ['./header-promotion.component.css']
+})
+export class HeaderPromotionComponent implements OnInit {
+  path:String = Path.url;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
